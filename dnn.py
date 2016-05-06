67aeab01da26abcede101f458fbe96d4b49e8ff9
huihui <1433whh@tongji.edu.cn>
2016-05-06 10:45:55 +0800

    add import

--- dnn.py
+++ dnn.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# dnn classifier
+
+import numpy
+import theano
+import theano.tensor as T
+import lasagne
+
 def do_train(data,n_layers = 3, n_hidden=1024):
     pass
 def do_classification_dnn(data, n_layers=3, n_hidden = 1024):
