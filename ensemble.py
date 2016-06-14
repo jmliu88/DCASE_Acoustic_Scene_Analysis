5ea6cb9edd6e995c158fea482c226686251ec3b2
James <you@example.com>
2016-06-15 01:57:17 +0800

    modify ensemble halfway

--- ensemble.py
+++ ensemble.py
@@ -4,6 +4,7 @@ from src.dataset import *
 from task1_scene_classification import *
 import sys
 from pprint import pprint
+import numpy as np
 if __name__ == "__main__":
 
     dataset = TUTAcousticScenes_2016_DevelopmentSet('data/')
@@ -11,13 +12,30 @@ if __name__ == "__main__":
     ensemble_yaml = sys.argv[1]
     ensemble_params = load_parameters(ensemble_yaml)
     yamls = ensemble_params['models'].split()
+    y_pred = []
     for i_model in yamls:
         i_params = load_parameters(i_model)
         i_params = process_parameters(i_params)
 
         result_path = i_params['path']['results']
-        result = do_gether_results(dataset, result_path, 'folds')
-    results = dataset
-    result = []
+        y_true, i_y_pred, result = do_gether_results(dataset, result_path, 'folds')
+        y_pred.append(i_y_pred)
 
+    preds = np.array(y_pred)
+    if ensemble_params['type'] == "majority_vote":
+        for i in range(preds.shape[1]):
+            i_sample = preds[:,i]
+            i_sample = i_sample.tolist()
+            dic={}
+            for i_pred in i_sample:
+                dic[i_pred]= i_sample.count(i_pred)
+
+            l = dic.keys()[0]
+            for k in dic:
+	        if dic[k]>dic[l]:
+	            l = k
+            pred.append(l)
+
+
+    sys.exit(1)
     do_system_evaluation(dataset,result,'folds')
