dfffdc6af41c1b5d51f2f962d09c701f3616fa4f
James <you@example.com>
2016-06-16 23:08:16 +0800

    ensemble done

--- do_system_evaluation.py
+++ do_system_evaluation.py
@@ -4,6 +4,13 @@ from src.dataset import *
 import task1_scene_classification
 import sys
 if __name__ == "__main__":
-    result = sys.argv[1]
+    result_file = sys.argv[1]
+    if os.path.splitext(result_file)[-1] == '.yaml':
+	params = task1_scene_classification.load_parameters(result_file)
+	params = task1_scene_classification.process_parameters(params)
+	result = params['path']['results']
+    else:
+        result = result_file
+    print result
     dataset = TUTAcousticScenes_2016_DevelopmentSet('data/')
     task1_scene_classification.do_system_evaluation(dataset,result,'folds')
--- ensemble.py
+++ ensemble.py
@@ -7,10 +7,37 @@ from pprint import pprint
 import numpy as np
 def get_prediction(result_filename):
 
+    results=[]
     with open(result_filename, 'rt') as f:
         for row in csv.reader(f, delimiter='\t'):
             results.append(row)
     return results
+def get_result_list(results_list):
+    result = []
+    for i in results_list:
+        result.append( get_prediction(i))
+    return result
+def vote(result_list):
+    pred = []
+    for i in range(len(result_list[1])):
+	i_sample = [result_list[x][i] for x in range(len(result_list))]
+	dic={}
+	for i_pred in i_sample:
+	    i_pred = '\t'.join(i_pred)
+	    dic[i_pred]= i_sample.count(i_pred)
+
+	l = dic.keys()[0]
+	for k in dic:
+	    if dic[k]>dic[l]:
+		l = k
+	pred.append(l)
+    return pred
+def write(savename,pred):
+    with open(savename,'w') as fid:
+	for i in pred:
+	    fid.write('%s\n'%(i))
+
+    
 if __name__ == "__main__":
 
     dataset = TUTAcousticScenes_2016_DevelopmentSet('data/')
@@ -22,30 +49,23 @@ if __name__ == "__main__":
     ensemble_params = load_parameters(ensemble_yaml)
     yamls = ensemble_params['models'].split()
     y_pred = []
+    results = []
+
+    result_path_list = []
     for i_file in range(1,5):
         for i_model in yamls:
             i_params = load_parameters(i_model)
             i_params = process_parameters(i_params)
 
             result_path = i_params['path']['results']
-            results.append(get_prediction(os.path.join(result_path,'results_fold%d.txt'%i_file)))
-
-        preds = np.array(results)
+            result_path_list.append( os.path.join(result_path,'results_fold%d.txt'%i_file))
+        result_list = get_result_list(result_path_list)
         if ensemble_params['type'] == "majority_vote":
-            for i in range(preds.shape[1]):
-                i_sample = preds[:,i]
-                i_sample = i_sample.tolist()
-                dic={}
-                for i_pred in i_sample:
-                    dic[i_pred]= i_sample.count(i_pred)
-
-                l = dic.keys()[0]
-                for k in dic:
-                    if dic[k]>dic[l]:
-                        l = k
-                pred.append(l)
-    import pdb; pdb.set_trace()
+            results = vote(result_list)
+            #results.append(get_prediction(os.path.join(result_path,'results_fold%d.txt'%i_file)))
+        save_file=os.path.join(save_path,'results_fold%d.txt'%i_file)
+        write(save_file,results)
 
 
-    sys.exit(1)
-    do_system_evaluation(dataset,result,'folds')
+
+    do_system_evaluation(dataset,save_path,'folds')
